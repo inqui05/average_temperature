5e67dbb88425aa7418d6891bf655a14c53f5d668
Artsiom Tratsiuk <inqui05@gmail.com>
2021-11-29 09:57:24 +0300

    fix: change environments files to secure keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
-    production: false,
-    googleMapsKey: 'AIzaSyAQCdhKcwHUbnkfkXeoVPI6za6qdvxufmo',
-    openWeatherMapKey: 'ab9208fbc5cfc5e1c2884975884d361e',
-  }
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
